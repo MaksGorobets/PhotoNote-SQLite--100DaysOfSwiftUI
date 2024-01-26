86804cb974507070c8c121ed64d0ad76572bd0f0
MaksGorobets <gorobets542@gmail.com>
2024-01-26 16:16:51 +0200

    Initial Commit

---
+++ PhotoNote/PhotoNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoNoteApp.swift
+//  PhotoNote
+//
+//  Created by Maks Winters on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
